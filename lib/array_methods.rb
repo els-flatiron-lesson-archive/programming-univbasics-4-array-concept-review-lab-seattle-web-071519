ef11155ed1a59fd2bce0be48a0b7e02b734e14ea
Helen Marshall <marshallhelenm@gmail.com>
2019-06-29 16:51:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,5 +18,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  lowest = ''
+  array.length.times { |index|
+    if array[index] > highest
+      highest = array[index]
+    end
+  }
+  return highest
 end
