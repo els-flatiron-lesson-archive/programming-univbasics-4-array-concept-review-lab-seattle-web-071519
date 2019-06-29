43b9e48db202ce98bcafe384289ca47236e716e9
Helen Marshall <marshallhelenm@gmail.com>
2019-06-29 16:53:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,7 +20,7 @@ end
 def find_min_value(array)
   lowest = array[0]
   array.length.times { |index|
-    if array[index] > lowest
+    if array[index] < lowest
       lowest = array[index]
     end
   }
